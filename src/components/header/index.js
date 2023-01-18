4806a44cbb9bd807694397ccff9e542fcbfe926e
HeitorUrbanetz <jhty123453@gmail.com>
2023-01-18 11:41:13 -0300

    terminado projeto

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,17 @@
-export const Header = () => {
+import { Link } from 'react-router-dom'
+
+export const Header = ({ back }) => {
   return (
     <div className="w-full bg-transparent h-16 p-4 pl-24 mt-4">
       <div className="flex w-96 justify-between">
+        {back ? (
+          <Link
+            to="/"
+            className="text-gray-700 cursor-pointer hover:text-white transition ease-in-out"
+          >
+            Voltar
+          </Link>
+        ) : null}
         <h1 className="text-white cursor-pointer">Heitor Urbanetz</h1>
         <a
           href="https://github.com/HeitorUrbanetz"
--- src/pages/anime/index.js
+++ src/pages/anime/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import { Header } from '../../components/header'
+import { Spinner } from '../../components/spinner'
 
 export const Anime = () => {
   const location = useLocation()
@@ -20,22 +22,45 @@ export const Anime = () => {
   }, [])
 
   return (
-    <div className="bg-secondary items-center  flex-col w-screen h-screen flex justify-center">
-      <div className="flex p-20 flex-row justify-center items-center text-white">
-        <img src={item.images?.jpg.large_image_url} alt={item.title} />
-        <div className="ml-20 ">
-          <div className="flex flex-row gap-2 items-center">
-            <h1 className="text-2xl font-bold">{item?.title}</h1>
-            <p className="text-sm text-yellow-400">Score: {item?.episodes}</p>
+    <>
+      {Object.keys(item).length === 0 ? (
+        <div className="bg-secondary items-center relative  flex-col w-screen h-screen flex justify-center">
+          <div className="absolute top-2 left-0">
+            <Header back />
           </div>
-          <div className='mt-10 pr-36'>
-          <p className='text-left text-sm block'>{item?.background}</p>
-          </div>
-          <h1 className='mt-10'>
-            Número de Episódios: <span className='text-red-500'>{item?.episodes}</span>
-          </h1>
+          <Spinner />
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="bg-secondary items-center  flex-col w-screen h-screen flex justify-center">
+          <Header back />
+          <div className="flex p-20 flex-row justify-center items-center text-white">
+            <img
+              className="w-60 h-90"
+              src={item.images?.jpg.large_image_url}
+              alt={item.title}
+            />
+            <div className="ml-20 ">
+              <div className="flex flex-row gap-2 items-center">
+                <h1 className="text-3xl font-bold">{item?.title}</h1>
+                <p className="text-sm text-yellow-400">
+                  Pontos: {item?.episodes}
+                </p>
+              </div>
+              <div className="mt-10 pr-36">
+                <p className="text-left text-lg block">{item?.background}</p>
+              </div>
+              <h1 className="mt-10">
+                Número de Episódios:{' '}
+                <span className="text-red-500">{item?.episodes}</span>
+              </h1>
+              <h1 className="absolute bottom-10 text-gray-700">
+                * as descrições são em inglês pois vem da api que está em
+                ingles.
+              </h1>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
